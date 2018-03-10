be04151f60fb14be75fd96ea446cc0977c35dab9
Alex19427 <alex19427@gmail.com>
2018-03-10 16:14:48 +0530

    new

--- q04_topic_modelling/tests/test.py
+++ q04_topic_modelling/tests/test.py
@@ -1,5 +1,5 @@
 import unittest
-from inspect import getargspec
+from inspect import getfullargspec
 import dill
 import pandas as pd
 from ..build import q04_topic_modelling
@@ -27,16 +27,16 @@ class Testing(unittest.TestCase):
         print(' ')
         print(' testing the arguements of the functions')
         print(' ')
-        self.args_student = getargspec(self.student_func).args
-        self.args_original = getargspec(self.solution_func).args
+        self.args_student = getfullargspec(self.student_func).args
+        self.args_original = getfullargspec(self.solution_func).args
         self.assertEqual(len(self.args_student), len(self.args_original),
                          "Expected argument(s) %d, Given %d" % (len(self.args_original), len(self.args_student)))
 
         # check the defaults of the function
 
     def test_defaults(self):
-        self.defaults_student = getargspec(self.student_func).defaults
-        self.defaults_solution = getargspec(self.solution_func).defaults
+        self.defaults_student = getfullargspec(self.student_func).defaults
+        self.defaults_solution = getfullargspec(self.solution_func).defaults
         self.assertEqual(self.defaults_student, self.defaults_solution,
                          "Expected default values do not match given default values")
 
