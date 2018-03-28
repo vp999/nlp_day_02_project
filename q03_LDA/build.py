95b4b5951b721f07392040686f2ee879ebcb965b
vp999 <patilvish@gmail.com>
2018-03-28 05:29:53 +0000

    Done

--- q03_LDA/build.py
+++ q03_LDA/build.py
@@ -5,6 +5,10 @@ from greyatomlib.nlp_day_02_project.q02_count_vectorizer_for_LDA.build import q0
 
 
 # Write your solution here :
-
-
-
+def q03_LDA(path):
+    matrix, feature_names = q02_count_vectorizer_for_LDA(path)
+#     vocab = feature_names
+    model =LatentDirichletAllocation(n_topics=20, random_state=1,learning_method='batch',max_iter=500)
+    model.fit(matrix)
+    topic_word = model.components_
+    return topic_word
