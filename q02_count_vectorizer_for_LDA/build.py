4f77ae4486c383cdae03bad98786c21ca1aedcec
vp999 <patilvish@gmail.com>
2018-03-28 05:13:08 +0000

    Done

--- q02_count_vectorizer_for_LDA/build.py
+++ q02_count_vectorizer_for_LDA/build.py
@@ -8,5 +8,8 @@ from greyatomlib.nlp_day_02_project.q01_load_data_tfidf.build import q01_load_da
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Write your solution here:
-
-
+def q02_count_vectorizer_for_LDA (path):
+    df, vectors ,features =  q01_load_data_tfidf(path)
+    vect = CountVectorizer(analyzer='word', ngram_range=(1,1), min_df=0, stop_words='english')
+    train_vectors = vect.fit_transform(df['talkTitle'])
+    return train_vectors, vect.get_feature_names()
